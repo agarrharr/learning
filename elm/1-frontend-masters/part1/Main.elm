65c1050b0c838e11da6311f9d3911012adb76d69
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-02-09 10:27:00 -0700

    Elm part1

--- elm/1-frontend-masters/part1/Main.elm
+++ elm/1-frontend-masters/part1/Main.elm
@@ -7,16 +7,12 @@ import Html.Attributes exposing (..)
 main =
     div [ class "content" ]
         [ header []
-            [ -- TODO wrap this text in an <h1>
-              text "ElmHub"
-            , span
-                [-- TODO give this span a class="tagline" attribute.
-                 --
-                 -- HINT: look at how our <div class="content"> does this above.
+            [ h1 []
+                [ text "ElmHub"
                 ]
-                [{- TODO put some text in here that says:
-                    "Like GitHub, but for Elm things."
-                 -}
+            , span
+                [ class "tagline" ]
+                [ text "Like GitHub, but for Elm things."
                 ]
             ]
         ]
