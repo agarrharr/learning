f3980c045fc0022bff86f91f509f8f210f78ec6c
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-02-12 10:00:45 -0700

    Add part5

---
+++ elm/1-frontend-masters/part5/Main.elm
@@ -0,0 +1,107 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
+
+
+type alias Model =
+    { query : String
+    , results : List SearchResult
+    }
+
+
+type alias SearchResult =
+    { id : Int
+    , name : String
+    , stars : Int
+    }
+
+
+type Msg
+    = SetQuery String
+    | DeleteById Int
+
+
+initialModel : Model
+initialModel =
+    { query = "tutorial"
+    , results =
+        [ { id = 1
+          , name = "TheSeamau5/elm-checkerboardgrid-tutorial"
+          , stars = 66
+          }
+        , { id = 2
+          , name = "grzegorzbalcerek/elm-by-example"
+          , stars = 41
+          }
+        , { id = 3
+          , name = "sporto/elm-tutorial-app"
+          , stars = 35
+          }
+        , { id = 4
+          , name = "jvoigtlaender/Elm-Tutorium"
+          , stars = 10
+          }
+        , { id = 5
+          , name = "sporto/elm-tutorial-assets"
+          , stars = 7
+          }
+        ]
+    }
+
+
+elmHubHeader : Html Msg
+elmHubHeader =
+    header []
+        [ h1 [] [ text "ElmHub" ]
+        , span [ class "tagline" ] [ text "Like GitHub, but for Elm things." ]
+        ]
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "content" ]
+        [ header []
+            [ h1 [] [ text "ElmHub" ]
+            , span [ class "tagline" ] [ text "Like GitHub, but for Elm things." ]
+            ]
+        , input
+            [ class "search-query"
+
+            -- TODO onInput, set the query in the model
+            , defaultValue model.query
+            ]
+            []
+        , button [ class "search-button" ] [ text "Search" ]
+        , ul [ class "results" ] (List.map viewSearchResult model.results)
+        ]
+
+
+viewSearchResult : SearchResult -> Html Msg
+viewSearchResult result =
+    li []
+        [ span [ class "star-count" ] [ text (toString result.stars) ]
+        , a [ href ("https://github.com/" ++ result.name), target "_blank" ]
+            [ text result.name ]
+        , button
+            -- TODO add an onClick handler that sends a DeleteById msg
+            [ class "hide-result" ]
+            [ text "X" ]
+        ]
+
+
+update : Msg -> Model -> Model
+update msg model =
+    -- TODO if we get a SetQuery msg, use it to set the model's query field,
+    -- and if we get a DeleteById msg, delete the appropriate result
+    model
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram
+        { view = view
+        , update = update
+        , model = initialModel
+        }
---
+++ elm/1-frontend-masters/part5/README.md
@@ -0,0 +1,22 @@
+Part 5
+======
+
+## Installation
+
+```bash
+elm-package install
+```
+
+(Answer `y` when prompted.)
+
+
+## Building
+
+```bash
+elm-live Main.elm --open --pushstate --output=elm.js
+```
+
+## References
+
+* [**case-expressions**](http://elm-lang.org/docs/syntax#conditionals)
+* [Union Types syntax reference](http://elm-lang.org/docs/syntax#union-types)
