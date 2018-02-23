ba0778de170747ccf7143b1c24508fd2bf7243e3
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-02-23 12:43:11 -0700

    Remove impure functions from class

--- ruby/2-imperative-to-oo-to-functional/build.rb
+++ ruby/2-imperative-to-oo-to-functional/build.rb
@@ -1,17 +1,13 @@
 # Vim command to create mapping to run
 # :map <leader>t :w\|:!touch a.html && rm -f *.html && ruby build.rb && ls -l *.html && cat *.html<cr>
-#
-# Looking at the OO code
-# Pull out all of the mutation
-# Pull out all of the access of external systems
-# I want this to be referentially transparent
-# Make Page an object composed of pure functions
 
 require "rdiscount"
 
 def main
   paths.each do |path|
-    Page.new(path).write_compiled
+    content = File.read(path)
+    page = Page.new(path, content)
+    File.write(page.new_path, page.compile)
   end
 end
 
@@ -20,28 +16,19 @@ def paths
 end
 
 class Page
-  attr_reader :path
+  attr_reader :path, :content
 
-  def initialize(path)
+  def initialize(path, content)
     @path = path
-  end
-
-  # Impure function
-  def read
-    File.read(path)
+    @content = content
   end
 
   def compile
-    RDiscount.new(read).to_html
+    RDiscount.new(content).to_html
   end
 
   def new_path
     path.sub(/\.md$/, ".html")
-  end
-
-  # Impure function
-  def write_compiled
-    File.write(new_path, compile)
   end
 end
 
