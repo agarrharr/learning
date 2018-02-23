34de0d47f9c14325407b434339862ce0770f5fa2
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-02-23 12:30:17 -0700

    Refactor towards OO

--- ruby/2-imperative-to-oo-to-functional/build.rb
+++ ruby/2-imperative-to-oo-to-functional/build.rb
@@ -1,16 +1,32 @@
+# Vim command to create mapping to run
+# :map <leader>t :w\|:!touch a.html && rm -f *.html && ruby build.rb && ls -l *.html && cat *.html<cr>
+
 require "rdiscount"
 
 def main
-  paths = Dir["*.md"]
   paths.each do |path|
-    content = File.read(path)
-    compiled = RDiscount.new(content).to_html
-    new_path = path.sub(/\.md$/, ".html")
-    File.write(new_path, compiled)
+    write_compiled(path)
   end
 end
 
-main
+def paths
+  Dir["*.md"]
+end
 
-# map key to run
-# :map <leader>t :w\|:!rm -f *.html && ruby build.rb && ls -l *.html && cat *.html<cr>
+def read(path)
+  File.read(path)
+end
+
+def compile(path)
+  RDiscount.new(read(path)).to_html
+end
+
+def new_path(path)
+  path.sub(/\.md$/, ".html")
+end
+
+def write_compiled(path)
+  File.write(new_path(path), compile(path))
+end
+
+main
