72d9d7468aa7c160a7c67a98288b947dae4afd3d
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-02-28 16:46:24 -0700

    Get input from user

--- ruby/4-well-grounded-rubyist/celsius_to_fahrenheit.rb
+++ ruby/4-well-grounded-rubyist/celsius_to_fahrenheit.rb
@@ -1,5 +1,6 @@
-celsius = 100
-fahrenheit = (celsius * 9 / 5) + 32
+print "Enter a celsius value: "
+celsius = gets
+fahrenheit = (celsius.to_i * 9 / 5) + 32
 print "The result is: "
 print fahrenheit
 puts "."
