e55165556cb5075dcdec03fa99b991fef8f109c4
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-03-21 10:11:03 -0600

    Finish password generator

--- ruby/6-password-generator/password_generator/lib/password_generator.rb
+++ ruby/6-password-generator/password_generator/lib/password_generator.rb
@@ -17,37 +17,36 @@ class PasswordGenerator
   end
 
   def generate
+    rest = length - punct - digit - upper.to_i
     if upper
-      default_sample = Sampler.new(LOWER, unique)
+      default_sample = Sampler.new(LOWER, unique, rest)
     else
-      default_sample = Sampler.new(ALPHA, unique)
+      default_sample = Sampler.new(ALPHA, unique, rest)
     end
-    punct_sampler = Sampler.new(PUNCT, unique)
-    digit_sampler = Sampler.new(NUMBER, unique)
-    upper_sampler = Sampler.new(UPPER, unique)
+    punct_sampler = Sampler.new(PUNCT, unique, punct)
+    digit_sampler = Sampler.new(NUMBER, unique, digit)
+    upper_sampler = Sampler.new(UPPER, unique, upper.to_i)
 
-    ((1..(length - punct - digit - upper.to_i)).map do
-      default_sample.sample
-    end + (1..punct).map do
-      punct_sampler.sample
-    end + (1..digit).map do
-      digit_sampler.sample
-    end + (1..upper.to_i).map do
-      upper_sampler.sample
-    end).shuffle.join
+    [default_sample, punct_sampler, digit_sampler, upper_sampler].map do |sampler|
+      sampler.sample
+    end.flatten.shuffle.join
   end
 
+end
+
   class Sampler
-    def initialize(ary, unique)
+    def initialize(ary, unique, count)
       @ary, @unique = ary.dup, unique
+      @count = count
     end
 
     def sample
-      if @unique
-        @ary.delete_at(rand(@ary.size))
-      else
-        @ary.sample
+      (1..@count).map do
+        if @unique
+          @ary.delete_at(rand(@ary.size))
+        else
+          @ary.sample
+        end
       end
     end
   end
-end
