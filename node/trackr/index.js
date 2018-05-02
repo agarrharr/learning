0ed482fd6770dd5c6ffbec1468bdce4b38b6b960
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2018-05-02 15:37:53 -0600

    Add date, isUrl, and ip

--- node/trackr/index.js
+++ node/trackr/index.js
@@ -1,14 +1,30 @@
 const express = require('express');
+const fs = require('fs');
+const { DateTime } = require('luxon');
+const isAbsoluteUrl = require('is-absolute-url');
+const requestIp = require('request-ip');
 
 const app = express();
 
-app.get('/', (request, result) => {
-  result.send('Hello worldz!');
+app.use(requestIp.mw())
+
+app.get(`/`, (request, result) => {
+  result.send(`Trackr`);
 });
 
-app.get('/redirect/:url*', (request, result) => {
+app.get(`/redirect/:url*`, (request, result) => {
   const url = `${request.params.url}${request.param(0)}`;
-  result.redirect(url);
+  const isUrl = isAbsoluteUrl(url);
+  const datetime = DateTime.local();
+  const ip = request.clientIp;
+
+  fs.appendFile(`log.csv`, `"${url}", "${isUrl}", "${datetime}", "${ip}"\n`);
+
+  if (isUrl) {
+    result.redirect(url);
+  } else {
+    result.send('Not a valid url');
+  }
 });
 
 const port = 8000;
