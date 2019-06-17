9f348b6fd000426b775090de6fc020a40d2c31cf
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2019-06-17 14:25:01 -0600

    Actually add cs unit tests

---
+++ cs/gradebook/test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var book = new Book("");
+
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
+
+            var result = book.GetStatistics();
+
+            Assert.Equal(85.6, result.Average, 1);
+            Assert.Equal(77.3, result.Low, 1);
+            Assert.Equal(90.5, result.High, 1);
+        }
+    }
+}
