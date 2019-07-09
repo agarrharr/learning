a469a82309945dcde1b402d2917f93dab71bd5b5
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2019-07-09 16:17:45 -0600

    Change label color as well

--- ios11/The Night Porter/The Night Porter/ViewController.swift
+++ ios11/The Night Porter/The Night Porter/ViewController.swift
@@ -12,6 +12,15 @@ class ViewController: UIViewController {
 
     @IBAction func changeBackground(_ sender: Any) {
         view.backgroundColor = UIColor.darkGray
+        
+        let everything = view.subviews
+        
+        for eachView in everything {
+            if eachView is UILabel {
+                let currentLabel = eachView as! UILabel
+                currentLabel.textColor = UIColor.white
+            }
+        }
     }
     
     override func viewDidLoad() {
