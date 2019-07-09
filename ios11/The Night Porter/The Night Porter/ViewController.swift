5e875667f56493fa56a0d04ddb75dade3109b7f6
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2019-07-09 16:10:02 -0600

    Add labels and a button to change the background

--- ios11/The Night Porter/The Night Porter/ViewController.swift
+++ ios11/The Night Porter/The Night Porter/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func changeBackground(_ sender: Any) {
+        view.backgroundColor = UIColor.darkGray
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
