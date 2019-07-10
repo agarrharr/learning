582ec77badc9495e17c5a470d0cd04a8c17c0196
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2019-07-09 23:01:03 -0600

    Use a table view

--- ios11/The Night Porter/The Night Porter/ViewController.swift
+++ ios11/The Night Porter/The Night Porter/ViewController.swift
@@ -8,20 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBAction func changeBackground(_ sender: Any) {
-        view.backgroundColor = UIColor.darkGray
-        
-        let everything = view.subviews
-        
-        for eachView in everything {
-            if eachView is UILabel {
-                let currentLabel = eachView as! UILabel
-                currentLabel.textColor = UIColor.white
-            }
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return dailyTasks.count
+        case 1:
+            return weeklyTasks.count
+        case 2:
+            return monthlyTasks.count
+        default:
+            return 0
         }
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        switch indexPath.section {
+        case 0:
+            cell.textLabel?.text = dailyTasks[indexPath.row]
+        case 1:
+            cell.textLabel?.text = weeklyTasks[indexPath.row]
+        case 2:
+            cell.textLabel?.text = monthlyTasks[indexPath.row]
+        default:
+            cell.textLabel?.text = "This shouldn't happen"
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Daily Tasks"
+        case 1:
+            return "Weekly Tasks"
+        case 2:
+            return "Monthly Tasks"
+        default:
+            return nil
+        }
+    }
+    
+    
+    let dailyTasks = ["Check all windows",
+                      "Check all doors",
+                      "Is the boiler fueled?",
+                      "Check the mailbox",
+                      "Empty trash containers",
+                      "If freezing, check water pipes",
+                      "Document \"strange and unusual\" occurences"]
+    
+    let weeklyTasks = ["Check inside all cabins",
+                       "Flush all lavatories in cabins",
+                       "Walk the perimeter of property"]
+    
+    let monthlyTasks = ["Test security alarm",
+                      "Test motion detectors",
+                      "Test smoke alarms"]
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
