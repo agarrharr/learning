19b999a4e2edccf730dbf39dc493ed7b1b3a8110
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2021-03-21 20:40:12 -0600

    Add swift stuff

---
+++ swift/2-codecademy/initials.swift
@@ -0,0 +1,13 @@
+print("  A")
+print(" A A")
+print("A   A")
+print("AAAAA")
+print("A   A")
+print("A   A")
+print("A   A")
+
+// TODO: Make a function for each letter that returns an array of strings for each line
+// https://content.codecademy.com/courses/learn-cpp/hello-world/block-letters-hint.png
+// Then make a function that takes in a string like "HELLO"
+// It should print out each letter by printing the first line of each array
+// followed by the second line from each array, and so on
--- swift/readme.md
+++ swift/readme.md
@@ -1,3 +1,4 @@
 # Swift
 
-- [] [Swift Fundamentals](https://app.pluralsight.com/library/courses/swift3-fundamentals/table-of-contents)
+- [ ] [Swift Fundamentals](https://app.pluralsight.com/library/courses/swift3-fundamentals/table-of-contents)
+- [ ] [Learn Swift on Codecademy](https://www.codecademy.com/learn/learn-swift) - [code](/swift/2-codecademy)
